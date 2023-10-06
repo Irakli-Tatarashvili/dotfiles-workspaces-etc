e9ab9d5ae0086f79d30107fe24c31282f97da4e2
Irakli Tatarashvili <tatarashviliirakli1@gmail.com>
2023-10-06 19:02:24 +0400

    update

---
+++ content/firefox/user.js
@@ -0,0 +1,15 @@
+// on linux go to about:profiles, find "default profile yes" and drop this file in root directory
+
+// Disable Pocket extension
+user_pref("extensions.pocket.enabled", false);
+
+// turn off video top blue popup warning
+user_pref("full-screen-api.warning.timeout", 0);
+
+
+
+
+
+// ON WINDOWS 10 ALSO CHANGE THOSE manually!:
+// full-screen-api.transition-duration.enter = "0 0"
+// full-screen-api.transition-duration.leave = "0 0"
