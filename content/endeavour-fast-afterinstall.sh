355c4ac4227bb1337499e5e6f1ecbd580a66bcc2
Irakli Tatarashvili <tatarashviliirakli1@gmail.com>
2023-10-13 15:22:39 +0400

    upd

--- content/endeavour-fast-afterinstall.sh
+++ content/endeavour-fast-afterinstall.sh
@@ -28,10 +28,15 @@ sudo systemctl enable bluetooth
 # activates timeshift autobackup
 sudo systemctl enable --now cronie.service
 
+# installing virtual box
+sudo pacman -Syu --needed linux-headers --noconfirm
 
+# choose virtualbox-host-modules-arch if asked.
+sudo pacman -S virtualbox virtualbox-guest-iso
+sudo modprobe vboxdrv
+yay -S virtualbox-ext-oracle
 
-
-
+# installing virtual box END
 
 
 
