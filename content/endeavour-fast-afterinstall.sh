a3e04af4d3c1fecb69d728258167633caa60ac2b
Irakli Tatarashvili <tatarashviliirakli1@gmail.com>
2024-01-10 12:19:23 +0400

    added new installations to endeavour-fast-afterinstall, made documentation better and added time bug fix that happens on linux when dual booting windows and linux

--- content/endeavour-fast-afterinstall.sh
+++ content/endeavour-fast-afterinstall.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-# IN-FILE DOCUMENTATION!
+# DESCRIPTION: after installing EndeavourOS, run this script for software installation and useful configurations.
 
-# after installing EndeavourOS run this script for fast installation of used software.
+# || unrelated to this script / things you have to do since the shell can't do them:
 
-# unrelated to this script / things you have to do since the shell can't do them:
 # - add a keyboard layout
 # - configure LibreOffice dark mode
 
-# Function to check the exit status and print an error message
+# || Function to check the exit status and print an error message
+
 check_exit_status() {
-  if [ $? -ne 0 ]; then
+  local status=$?
+  if [ $status -ne 0 ]; then
     echo "Error: $1 failed."
     exit 1
   fi
 }
 
+# || Installing pacman and AUR packages
+
 # installs useful pacman packages
 sudo pacman -S --noconfirm xclip nodejs npm ksysguard ffmpegthumbs timeshift dotnet-sdk aspnet-runtime nuget
-check_exit_status "Pacman package installation"
+check_exit_status "yacman installation of packages"
 
-# installs vscode
-yay -S --noconfirm visual-studio-code-bin
-check_exit_status "Visual Studio Code installation"
+# installs useful yay packages
+yay -S --noconfirm visual-studio-code-bin figma-linux
+check_exit_status "yay's installation of AUR packages"
 
-# installs useful npm global packages
-sudo npm install -g typescript @vue/cli
-check_exit_status "NPM package installation"
-
-# installs vue template
-dotnet new install Microsoft.AspNetCore.SpaTemplates::*
-dotnet new install Vue.Simple.Template
-check_exit_status "Vue template installation"
+# || other system configurations
 
 # activates bluetooth on EndeavourOS
 sudo systemctl start bluetooth
@@ -40,8 +36,13 @@ check_exit_status "Bluetooth activation"
 sudo systemctl enable bluetooth
 check_exit_status "Bluetooth enabling"
 
+# fixing time on linux
+# https://www.howtogeek.com/323390/how-to-fix-windows-and-linux-showing-different-times-when-dual-booting/
+timedatectl set-local-rtc 1 --adjust-system-clock
+check_exit_status "Linux Time got fixed"
+
 # activates timeshift autobackup
 sudo systemctl enable --now cronie.service
 check_exit_status "Timeshift autobackup activation"
 
-# now save whatever you are doing and restart the computer!
+# now save whatever you are doing and reboot the computer!
